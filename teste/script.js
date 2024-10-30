73de85d8be4d576e4fe0ec0b2f18345a52e7e8fd
MuriloQuevedo <murilo.quev@gmail.com>
2024-10-30 08:30:57 -0300

    js

---
+++ teste/script.js
@@ -0,0 +1,33 @@
+function alterarTitulo() {
+    const titulo = document.getElementById("titulo");
+    titulo.textContent = titulo.textContent === "Meu Título"
+        ? "Título Alterado"
+        : "Meu Título";
+}
+
+function alterarParagrafo() {
+    const paragrafo = document.getElementById("paragrafo");
+    paragrafo.style.color = paragrafo.style.color === "gray"
+        ? "blue"
+        : "gray";
+}
+function tocarAudio() {
+    const audio = new Audio("die.wav"); // Certifique-se de que o arquivo está no mesmo diretório
+    audio.play();
+}
+
+function alterarBotoes() {
+    const botao2 = document.getElementById("botao2");
+    const botao3 = document.getElementById("botao3");
+    
+
+    // Toggle classes
+    botao2.classList.toggle("active");
+    botao3.classList.toggle("active");
+
+
+}
+function trocarImagem() {
+    const imagem = document.getElementById("imagem");
+    imagem.src = imagem.src.endsWith("random=1") ? "https://picsum.photos/200/200?random=2" : "https://picsum.photos/200/200?random=1";
+}
