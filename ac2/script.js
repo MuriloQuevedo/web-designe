b230f5254820655836c15c13462be33b7e9585b0
MuriloQuevedo <murilo.quev@gmail.com>
2024-11-06 08:41:31 -0300

    ac2

--- ac2/script.js
+++ ac2/script.js
@@ -1,21 +1,21 @@
-function addCard(event) {
+function Card(event) {
   event.preventDefault();
   const titulo = document.getElementById('titulo').value;
-  const cardContainer = document.getElementById('card-container');
+  const cardContainer = document.getElementById('cards');
 
   const colDiv = document.createElement('div');
   colDiv.className = 'col-md-3';
 
   
-  function generateRandomImageUrl() {
-    const randomNum = Math.floor(Math.random() * 1000); // 
+  function imagemalaatoria() {
+    const randomNum = Math.floor(Math.random() * 1000); 
     return `https://picsum.photos/150?random=${randomNum}`;
   }
 
   
   colDiv.innerHTML = `
   <div class="card mb-4">
-    <img src="${generateRandomImageUrl()}" class="card-img-top" alt="Imagem do Card">
+    <img src="${imagemalaatoria()}" class="card-img-top" alt="Imagem do Card">
     <div class="card-body text-center">
       <h5 class="card-title">${titulo}</h5>
       <button class="btn btn-danger" onclick="removeCard(this)">Apagar</button>
@@ -29,11 +29,11 @@ function addCard(event) {
   document.getElementById('titulo').value = '';
 }
 
-function removeCard(button) {
+function limpar(button) {
   button.closest('.col-md-3').remove();
 }
 
-function removeAllCards() {
-  const cardContainer = document.getElementById('card-container');
+function limpar() {
+  const cardContainer = document.getElementById('cards');
   cardContainer.innerHTML = '';
 }
