fc77291a1b459152b098541812d1a56f60410f13
MuriloQuevedo <murilo.quev@gmail.com>
2024-11-06 08:33:56 -0300

    ac2

--- ac2/script.js
+++ ac2/script.js
@@ -6,17 +6,25 @@ function addCard(event) {
   const colDiv = document.createElement('div');
   colDiv.className = 'col-md-3';
 
+  
+  function generateRandomImageUrl() {
+    const randomNum = Math.floor(Math.random() * 1000); // 
+    return `https://picsum.photos/150?random=${randomNum}`;
+  }
+
+  
   colDiv.innerHTML = `
-      <div class="card mb-4">
-        <img src="https://picsum.photos/150" class="card-img-top" alt="Imagem do Card">
-        <div class="card-body text-center">
-          <h5 class="card-title">${titulo}</h5>
-          <button class="btn btn-danger" onclick="removeCard(this)">Apagar</button>
-        </div>
-      </div>
-    `;
+  <div class="card mb-4">
+    <img src="${generateRandomImageUrl()}" class="card-img-top" alt="Imagem do Card">
+    <div class="card-body text-center">
+      <h5 class="card-title">${titulo}</h5>
+      <button class="btn btn-danger" onclick="removeCard(this)">Apagar</button>
+    </div>
+  </div>
+`;
 
   cardContainer.appendChild(colDiv);
+
 
   document.getElementById('titulo').value = '';
 }
